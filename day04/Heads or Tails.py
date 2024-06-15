6a436e43053efd7a830ca58d48a8a329b2dd2ab9
tudor.marica <tudor.code@gmail.com>
2024-06-15 15:22:08 +0300

    Update Heads or Tails.py

--- day04/Heads or Tails.py
+++ day04/Heads or Tails.py
@@ -1,1 +1,14 @@
+import random
 
+
+print("Welcome to Heads or Tails!")
+print("Toss the coin")
+
+number = random.randint(0,1)
+
+print(number)
+
+if number == 0:
+  print("Tails")
+else:
+  print("Heads")
